1fce98f9af7959fcb94c2022b05ebfea8a83bfdf
Anish <anish@starclinch.com>
2021-05-06 01:12:43 +0530

    port

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/irfan/Downloads/portirfan/git/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-lekoarts-gatsby-theme-cara-templates-cara-tsx": hot(preferDefault(require("/home/irfan/Downloads/portirfan/git/src/@lekoarts/gatsby-theme-cara/templates/cara.tsx")))
+}
+
